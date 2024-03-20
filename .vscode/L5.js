7a690c517cac801d0181dc1a6fe498322b161d4c
Laylasi <157524500+Laylasi@users.noreply.github.com>
2024-03-20 16:16:44 -0400

    Update

--- .vscode/L5.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('contactForm').addEventListener('submit', function(event) {
-  event.preventDefault();
-  const formData = new FormData(event.target);
-  const data = Object.fromEntries(formData.entries());
-  localStorage.setItem('contactFormData', JSON.stringify(data));
-  alert('Form submitted successfully!');
-});
