9d1292324cc02a91b78ea5b5f60569cd1eb4a53b
Laylasi <157524500+Laylasi@users.noreply.github.com>
2024-03-31 14:12:57 -0400

    Updated with centered photos

---
+++ PHP.php
@@ -0,0 +1,9 @@
+<?php
+// Retrieve the form data
+$name = $_POST['name'];
+$email = $_POST['email'];
+
+// Process the form data (in this example, we'll just print it out)
+echo "Name: " . $name . "<br>";
+echo "Email: " . $email;
+?>
