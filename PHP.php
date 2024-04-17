9b373b6c8989e4826224e4c78f8831a4c1749e66
Laylasi <157524500+Laylasi@users.noreply.github.com>
2024-04-17 14:54:52 -0400

    Updated

--- PHP.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Retrieve the form data
-$name = $_POST['name'];
-$email = $_POST['email'];
-
-// Process the form data (in this example, we'll just print it out)
-echo "Name: " . $name . "<br>";
-echo "Email: " . $email;
-?>
